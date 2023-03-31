91e2b0a59a9afa96a81c5090d8a03582fd72ae68
sushantkr961 <sushantonly961@gmail.com>
2023-03-31 23:02:23 +0530

    date and day added

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -2,14 +2,20 @@ let hh = document.querySelector(".hh");
 let mm = document.querySelector(".mm");
 let ss = document.querySelector(".ss");
 let am_pm = document.querySelector(".am_pm");
+let date = document.querySelector(".date");
+let day = document.querySelector(".day");
 
 let liveTime = () => {
   let currentDate = new Date();
-  //   console.log(currentDate);
+  // console.log(currentDate.toString().slice(0, 4));
+  // console.log(currentDate.toLocaleDateString());
+  // console.log(currentDate.toString().slice(4, 15));
+  day.textContent = currentDate.toString().slice(0, 4);
+  date.textContent = currentDate.toLocaleDateString();
   let hour = currentDate.getHours();
   let minute = currentDate.getMinutes();
   let second = currentDate.getSeconds();
-  //   console.log("hour", hour, "minute", minute, "second", second);
+  // console.log("hour", hour, "minute", minute, "second", second);
 
   /** converting in 1-12 only if hour is >12 */
   hour = hour >= 12 ? hour - 12 : "";
@@ -25,7 +31,7 @@ let liveTime = () => {
   ss.textContent = second;
 
   /** converting AM and PM */
-  hour >= 12 ? (am_pm.textContent = "PM") : (am_pm.textContent = "AM");
+  hour = hour >= 12 ? (am_pm.textContent = "AM") : (am_pm.textContent = "PM");
 };
 
 setInterval(liveTime, 1000);
