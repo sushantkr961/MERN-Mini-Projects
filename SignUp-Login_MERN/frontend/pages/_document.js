0c2ee9e0a90cc301fa43be374589fb36befe18db
sushantkr961 <sushantonly961@gmail.com>
2023-04-12 21:55:04 +0530

    some error resolved

--- SignUp-Login_MERN/frontend/pages/_document.js
+++ SignUp-Login_MERN/frontend/pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap"
           rel="stylesheet"
