d7441181380b2f65625c5def15f1e0c6057a4883
Sushant <sushantonly961@gmail.com>
2023-05-05 15:36:20 +0530

    footer and testimonial added

---
+++ selectMyCollegePHP/foot.php
@@ -0,0 +1,78 @@
+<!-- footer -->
+<div class="foot overflow-hidden" style="background: #002f26 0% 0% no-repeat padding-box;
+  border-radius: 50px 50px 0px 0px;">
+    <div class="container my-4">
+        <div class="row">
+            <div class="d-none d-lg-block col-5 my-4 bgr-gr rounded-start-5 d-flex flex-column justify-content-center align-items-center px-4 py-3">
+                <h3 class="text-center fw-bold fs-1">Select my college</h3>
+                <div class="text-center mb-2">
+                    <i class="fa-solid fa-house-chimney fs-5 me-4"></i>
+                    <i class="fa-brands fa-linkedin-in fs-3 me-4"></i>
+                    <i class="fa-brands fa-facebook-f fs-5 me-4"></i>
+                    <i class="fa-brands fa-instagram fs-4 me-4"></i>
+                    <i class="fa-brands fa-youtube fs-4 me-4"></i>
+                </div>
+                <p class="foot-p">
+                    Choosing the right college or university can be a challenging and overwhelming task for many
+                    students. That’s why our platform offers a comprehensive set of resources and tools to help students
+                    make informed decisions about their higher education.
+                </p>
+                <p class="foot-p">Our platform provides detailed information on a wide range of colleges and
+                    universities, including their academic programs, campus culture, location, and other relevant
+                    factors that can impact a student’s decision-making process. We also offer interactive tools and
+                    resources that allow students to compare different schools and programs based on their unique needs
+                    and preferences.
+                </p>
+                <div class="text-center">
+                    <button class="btn mb-3 text-white fs-4" style="background-color:#002F26" onclick="document.getElementById('choose_college').scrollIntoView({ behavior: 'smooth',block:'center' });">Check my eligibility</button>
+                </div>
+            </div>
+            <div class="col-lg-7 col-12 my-4 px-3 px-md-5">
+                <p class="text-white text-center">Subscribe for latest news related to college placement and exams</p>
+                <div class="input-group mb-3 row p-1 bg-white rounded-pill m-auto">
+                    <input type="text" class="form-control shadow-none border-0 col-9" placeholder="Email ID" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="text-white col-3 rounded-pill" style="right: 1px;background-color: #002f26;border:#002F26;" type="button" id="button-addon2">Submit</button>
+                </div>
+                <div class="row text-white">
+                    <div class="col-6">
+                        <h4>Top MBA colleges</h4>
+                        <div>
+                            <p>MBA Colleges in Delhi</p>
+                            <p>MBA Colleges in Delhi NCR</p>
+                            <p>MBA Colleges in Mumbai</p>
+                            <p>MBA Colleges in Hyderabad</p>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <h4>Top Engineering colleges</h4>
+                        <p>Engineering Colleges in Pune</p>
+                        <p>Engineering Colleges in Agra</p>
+                        <p>Engineering Colleges in Patna</p>
+                        <p>Engineering Colleges in Delhi</p>
+                    </div>
+                    <div class="col-6">
+                        <h4>Top Medical colleges</h4>
+                        <p>Medical Colleges in Meerut</p>
+                        <p>Medical Colleges in Maharastra</p>
+                        <p>Medical Colleges in Bhopal</p>
+                        <p>Medical Colleges in Thane</p>
+                    </div>
+                    <div class="col-6">
+                        <h4>Top BCA/BBA colleges</h4>
+                        <p>BCA/BBA Colleges in Indore</p>
+                        <p>BCA/BBA Colleges in Kerala</p>
+                        <p>BCA/BBA Colleges in Allahabad</p>
+                        <p>BCA/BBA Colleges in Noida</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class="text-white text-center mb-4">All right reserved Pantheon Digital @2023 <span> Terms of use | Privacy Policy | Cookie Policy</span></p>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
