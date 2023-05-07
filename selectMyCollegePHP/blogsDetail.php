1ac76ef533410c6b249ed0697041459d66f9ec07
Sushant <sushantonly961@gmail.com>
2023-05-07 15:37:11 +0530

    blogdetail added

---
+++ selectMyCollegePHP/blogsDetail.php
@@ -0,0 +1,86 @@
+<?php include 'head.php'; ?>
+<div class="container-fluid">
+    <div class="border border-2 rounded-3 p-3">
+        <div class="d-flex col-12 flex-column flex-md-row justify-content-evenly my-2 align-items-center">
+            <div class="col-12 col-md-5 d-flex align-content-center justify-content-center">
+                <img src="image/ci1.png" class="border border-1 img-fluid shadow-lg" alt="imgae" />
+            </div>
+            <div class="mx-2 p-2 col-12 col-md-7">
+                <h2 class="my-3">What are the best colleges in Delhi for B-Tech student</h2>
+                <p>Delhi is home to several prestigious colleges that offer B-Tech programs. Some of th best colleges in Delhi for delhi is home to several prestigious colleges that offer B-Tech programs. Some of the best colleges in Delhi for b-Tech students are... <span style="color:#33A500;margin-left:30px;cursor: pointer; "> Read More</span></p>
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <p style="color:#002F26;">Published on: <span style="color:#F7931E;"> 25, April 2023 </span></p>
+                        <p style="color:#002F26;">Publisher Name: <span style="color:#F7931E;"> Somnath Banerjee</span></p>
+                    </div>
+                    <div class="px-5 d-flex">
+                        <i class="fa-solid fa-share-from-square mx-1 fs-4" style="cursor: pointer;"></i>
+                        <i class="fa-regular fa-heart mx-1 fs-4" style="cursor: pointer;"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-5">
+            <p>Delhi is home to several prestigious colleges that offer B-Tech programs. Some of the best colleges in Delhi for B-Tech students are:</p>
+            <h5 class="fw-bold">1. Indian Institute of Technology Delhi (IIT Delhi)</h5>
+            <p>IIT Delhi is a premier engineering institute in India and is known for its academic excellence and research facilities.</p>
+            <h5 class="fw-bold">2. Delhi Technological University (DTU)</h5>
+            <p> DTU, formerly known as Delhi College of Engineering, is a government-funded institution that offers B-Tech programs in various disciplines.</p>
+            <h5 class="fw-bold">3. Netaji Subhas University of Technology (NSUT)</h5>
+            <p>NSUT, formerly known as Netaji Subhas Institute of Technology, is a leading engineering college in Delhi that offers B-Tech programs in several disciplines.</p>
+            <h5 class="fw-bold">4. Indraprastha Institute of Information Technology Delhi (IIIT-Delhi)</h5>
+            <p> IIIT-Delhi is a specialised institute that focuses on information technology and related areas.</p>
+            <h5 class="fw-bold">5. Guru Gobind Singh Indraprastha University (GGSIPU)</h5>
+            <p> GGSIPU is a state university that offers B-Tech programs in various disciplines.</p>
+            <h5 class="fw-bold">6. Jamia Millia Islamia </h5>
+            <p> Jamia Millia Islamia is a central university located in Delhi that offers B-Tech programs in various disciplines such as Mechanical, Civil, and Electrical Engineering. It has state-of-the-art laboratories, research centers, and a dedicated faculty.</p>
+            <h5 class="fw-bold">7. Delhi University</h5>
+            <p> Delhi University is one of the oldest universities in India and offers B-Tech programs in Computer Science, Electronics, and Communication Engineering. It is known for its excellent academic reputation and research facilities.</p>
+            <h5 class="fw-bold"> 8. National Institute of Technology Delhi (NIT Delhi)</h5>
+            <p> NIT Delhi is a new entrant in the list of top engineering colleges in Delhi. It offers B-Tech programs in Computer Science, Electronics and Communication Engineering, and Electrical and Electronics Engineering. It has a well-equipped campus with modern facilities and experienced faculty.</p>
+            <h5 class="fw-bold">9. Amity School of Engineering and Technology </h5>
+            <p>Amity School of Engineering and Technology is a private engineering college that offers B-Tech programs in several disciplines. It has a large and diverse student population and a modern campus with excellent academic facilities.</p>
+            <h5 class="fw-bold"> 10. Jaypee Institute of Information Technology (JIIT)</h5>
+            <p> JIIT is a private engineering college located in Noida, near Delhi. It offers B-Tech programs in Computer Science, Electronics and Communication Engineering, and Information Technology. It has a strong focus on research and innovation and a modern campus with state-of-the-art facilities. These are some of the top colleges for B-Tech students in Delhi. All these colleges offer excellent academic facilities, experienced faculty, and a vibrant campus life, making them an ideal choice for students who want to pursue a career in engineering. These colleges have excellent academic facilities, experienced faculty, and state-of-the-art infrastructure, making them ideal choices for B-Tech students in Delhi.</p>
+        </div>
+        <div class="form-floating mx-5 position-relative">
+            <textarea class="form-control rounded-3" placeholder="Leave a comment here" id="floatingTextarea" style="height: 200px;"></textarea>
+            <label for="floatingTextarea">Comments</label>
+            <button class="btn btn-primary position-absolute bottom-0 end-0 m-2" style="width: 100px;">Submit</button>
+        </div>
+        <div class="mx-5">
+            <h6 class="text-decoration-underline">Recent Comments</h6>
+            <div class="d-flex justify-content-between">
+                <p><span class="fw-bold" style="color:#002F26;">Riya Jha commented: </span>Riya Jha commented: Amazing information regarding this topic, thank you</p><span style="color:#F7931E;">Reply <i class="fa-regular fa-heart px-1"></i></span>
+            </div>
+            <div class="form-floating position-relative">
+                <textarea class="form-control rounded-3" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+            </div>
+        </div>
+        <div class="container">
+            <h1 class="text-decoration-underline mx-5 text-center">Suggested Posts</h1>
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="mb-2">
+                        <img class="rounded-4" src="image/College Images-01.png" alt="image" width="100%" height="100%" />
+                    </div>
+                    <p class="text-wrap text-start">What are the best colleges in Delhi for MBA</p>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="mb-2">
+                        <img class="rounded-4" src="image/College Images-01.png" alt="image" width="100%" height="100%" />
+                    </div>
+                    <p class="text-wrap text-start">What are the best colleges in Delhi for Engineering</p>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="mb-2">
+                        <img class="rounded-4" src="image/College Images-01.png" alt="image" width="100%" height="100%" />
+                    </div>
+                    <p class="text-wrap text-start">What are the best colleges in Delhi for Medical</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php include 'foot.php'; ?>
